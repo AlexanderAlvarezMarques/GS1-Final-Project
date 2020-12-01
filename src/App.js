3f2683bd36f5fc0013a20b00f4de7a434aa24366
Viviana Benitez <magi61012@gmail.com>
2020-12-01 15:48:24 +0000

    conection database done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { useState } from 'react';
+import React from 'react'
+import {auth} from './firebaseConfig';
 
 function App() {
+
+
+  /**
+   * Explica el hook de useState, la inicializacion y que se pueden usar varios estados en el componente,
+   * que es mejor que cada componente controle susestado y no un estado global.
+   * 
+   * Explica tambien la destructuracion de objetos en Javascript o Typescript (obtener datos desde un array,
+   * porque al final el useState nos devuelve un array de 2 posiciones, primero con el estado y el metodo 
+   * para actualizarse)
+   */
+  const [user, setUser] = useState([]);
+  const [isSignIn, setSignIn] = useState(false);
+
+  function login() {
+    /**
+     * Explica aqui que importas el auth y lo configuras en firebaseConfig.js, y el then es por lo de las promesas
+     * que es una funciona asincrona. Y cuando se cumple satisfactoriamente actualizas el estado. De esta
+     * forma se actualiza la vista.
+     */
+    auth.signInWithEmailAndPassword("magi61012@gmail.com", "123456")
+    .then(operation => {
+      setSignIn(true);
+      setUser(operation.user);
+      console.log(operation.user);
+    })
+  }
+
+  function logout() {
+    auth.signOut();
+    setUser([]);
+    setSignIn(false);
+  }
+
+
+  /**
+   * Aqui que solo pueden devolver un componente, si no quieren usar un div, pues React.Fragment.
+   * El onClick y que para hacer un if tiene que ser con el ternario.
+   */
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      { isSignIn ? 
+      <React.Fragment>
+        <button onClick={logout}>Cerrar sesión</button> 
+        <h2>Bienvenido a casa: {user.email}</h2>
+      </React.Fragment>
+      : 
+        <button onClick={login}>Iniciar sesión</button>
+      }
     </div>
   );
 }
