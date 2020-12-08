fcee04b8be34e80712adf6717606e5fa031844f0
DanielNaranjo <dnartor.98@gmail.com>
2020-12-08 17:33:49 +0000

    .

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import { useState, Fragment } from 'react';
-import React from 'react'
-import {auth} from './firebaseConfig';
-import SignUp from './componentes/SignUp';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import PagePrincipal from './componentes/PagePrincipal';
-import LogIn from './componentes/LogIn';
+import { useState, Fragment } from "react";
+import React from "react";
+import { auth } from "./firebaseConfig";
+import SignUp from "./componentes/SignUp";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import PagePrincipal from "./componentes/PagePrincipal";
+import LogIn from "./componentes/LogIn";
+import styled from "@emotion/styled";
+
+const Contenedor = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+`;
 
 function App() {
-
-
   /**
    * Explica el hook de useState, la inicializacion y que se pueden usar varios estados en el componente,
    * que es mejor que cada componente controle susestado y no un estado global.
-   * 
+   *
    * Explica tambien la destructuracion de objetos en Javascript o Typescript (obtener datos desde un array,
-   * porque al final el useState nos devuelve un array de 2 posiciones, primero con el estado y el metodo 
+   * porque al final el useState nos devuelve un array de 2 posiciones, primero con el estado y el metodo
    * para actualizarse)
    */
   const [user, setUser] = useState([]);
@@ -26,12 +30,13 @@ function App() {
      * que es una funciona asincrona. Y cuando se cumple satisfactoriamente actualizas el estado. De esta
      * forma se actualiza la vista.
      */
-    auth.signInWithEmailAndPassword("pepe@gmail.com", "elpepe")
-    .then(operation => {
-      setSignIn(true);
-      setUser(operation.user);
-      console.log(operation.user);
-    })
+    auth
+      .signInWithEmailAndPassword("pepe@gmail.com", "elpepe")
+      .then((operation) => {
+        setSignIn(true);
+        setUser(operation.user);
+        console.log(operation.user);
+      });
   }
 
   function logout() {
@@ -40,34 +45,34 @@ function App() {
     setSignIn(false);
   }
 
-
   /**
    * Aqui que solo pueden devolver un componente, si no quieren usar un div, pues React.Fragment.
    * El onClick y que para hacer un if tiene que ser con el ternario.
    */
   return (
-      <Router>
-        <Switch>
-          <Route exact path="/" component={PagePrincipal}/>
-          <Route exact path="/signUp" component={SignUp}/>
-          <Route exact path="/logIn" component={LogIn}/>
-          {/* <Route exact path="/login" component={Login}/> */}
-        </Switch>
-      </Router>
-      // {/* <SignIn/> */}
-      // {/* { isSignIn ? 
-      // <React.Fragment>
-      //   <button onClick={logout}>Cerrar sesión</button> 
-      //   <h2>Bienvenido a la aplicacion: {user.email}</h2>
-      // </React.Fragment>
-      // : 
-      // <Fragment>
-      //     <button onClick={login}>Iniciar sesión</button>
-      //     <Link to={'/signIn'}>Crear cuenta</Link>
-      // </Fragment>
-        
-      // }       */}
+    <Router>
+      <Switch>
+        <Contenedor>
+        <Route exact path="/" component={PagePrincipal} />
+        <Route exact path="/signUp" component={SignUp} />
+        <Route exact path="/logIn" component={LogIn} />
+        </Contenedor>
+        {/* <Route exact path="/login" component={Login}/> */}
+      </Switch>
+    </Router>
+    // {/* <SignIn/> */}
+    // {/* { isSignIn ?
+    // <React.Fragment>
+    //   <button onClick={logout}>Cerrar sesión</button>
+    //   <h2>Bienvenido a la aplicacion: {user.email}</h2>
+    // </React.Fragment>
+    // :
+    // <Fragment>
+    //     <button onClick={login}>Iniciar sesión</button>
+    //     <Link to={'/signIn'}>Crear cuenta</Link>
+    // </Fragment>
 
+    // }       */}
   );
 }
 
--- src/componentes/LogIn.js
+++ src/componentes/LogIn.js
@@ -1,25 +1,99 @@
 import React, { Fragment, useState } from "react";
 import { Router, useHistory } from "react-router-dom";
 import { auth, firestore } from "../firebaseConfig";
+import styled from "@emotion/styled";
+
+const contenedor = styled.div`
+  display: flex;
+  margin-bottom: 1rem;
+  align-items: center;
+  background-color: #ff5733;
+  width: 100%;
+`;
+const Label = styled.label`
+  flex: 0 0 100px;
+  width: 100%;
+`;
+const Boton = styled.button`
+  background-color: #00838f;
+  font-size: 10px;
+  width: 50%;
+  padding: 1rem;
+  color: #fff;
+  text-transform: uppercase;
+  font-weight: bold;
+  border: none;
+  &:hover {
+    background-color: #26c6da;
+    cursor: pointer;
+  }
+`;
 
 const LogIn = () => {
-  //para rediccionar
+  //para redireccionar
   let history = useHistory();
 
+  const [datos, guardarDatos] = useState({
+    userName: "",
+    passwd: "",
+  });
+  const [error, guardarError] = useState(false);
+
+  
+
+  // extrarer valores
+  const { userName, passwd } = datos;
+
+  const obtenerDatos = (e) => {
+    guardarDatos({
+      ...datos,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  //  cuando el usuario pulsa submit
+  const iniciarSesion = (e) => {
+    e.preventDefault();
+
+    if (userName.trim() == "" || passwd.trim() == "") {
+      guardarError(true);
+      return;
+    }
+    guardarError(false);
+  };
 
   //    HTML
-  return(
-      <Fragment>
-          <h2> Log In</h2>
-          <form>
-              <label>Nombre de usuario: </label>
-              <input type="text" name="userName"/>
-              <br></br>
-              <label>Contraseña</label>
-              <input type="password" name="passwd"/>
-          </form>
-
-      </Fragment>
-  )
+  return (
+    <Fragment>
+      <form onSubmit={iniciarSesion}>
+      <h2> Log In</h2>
+      
+        <contenedor>
+          <Label>Nombre de usuario:&nbsp;</Label>
+          <input
+            type="text"
+            name="userName"
+            value={userName}
+            onChange={obtenerDatos}
+          />
+        </contenedor>
+        <br></br>
+        <contenedor>
+          <Label>
+            Contraseña:&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;
+          </Label>
+          <input
+            type="password"
+            name="passwd"
+            value={passwd}
+            onChange={obtenerDatos}
+          />
+        </contenedor>
+        <br></br>
+        <Boton type="submit">Iniciar sesión</Boton>
+      </form>
+    </Fragment>
+  );
 };
 export default LogIn;
