25b2f80cb1d350c5f54b770fd441f632cc63797d
Alesa <alesaof@gmail.com>
2020-12-12 18:51:35 +0000

    Incidencia_V0.1

--- src/componentes/Incidence.js
+++ src/componentes/Incidence.js
@@ -3,25 +3,74 @@ import { Router, useHistory } from "react-router-dom";
 //import { auth, firestore } from "../firebaseConfig";
 
 const Incidence = () => {
-  //para rediccionar
-  let history = useHistory();
+    //para rediccionar
+    let history = useHistory();
 
+    const [datos, guardarDatos] = useState({
+        issue: "",
+        dateIssue: "",
+        incidenceContext: "",
+      });
+    const [error, guardarError] = useState(false);
+    const { issue, dateIssue, incidenceContext } = datos;
+    
+    
+    const obtenerDatos = (e) => {
+        guardarDatos({
+          ...datos,
+          [e.target.name]: e.target.value,
+        });
+      };
 
-  //    HTML
-  return(
-      <Fragment>
-          <h2> Incidencia</h2>
-          <form>
-              <label>Asunto: </label>
-              <input type="text" name="issue"/>
-              <br></br>
-              <label>Fecha: </label>
-              <input type="text" name="issue"/>
-              <br></br>
-              <label>Tema de incidencia: </label>
-              <input type="text" name="incidence"/>
-          </form>
-      </Fragment>
-  )
+      const enviarIncidencia = (e) => {
+        e.preventDefault();
+    
+        if (issue.trim() == "" || dateIssue.trim() == "" || incidenceContext.trim() == "") {
+          guardarError(true);
+          return;
+        }
+        guardarError(false);
+      };
+
+    //    HTML
+    return(
+        <Fragment>
+        <form onSubmit={enviarIncidencia}>
+        <h2> Incidencia</h2>
+        <contenedor>
+          <h3>Asunto:&nbsp;</h3>
+          <input
+            type="text"
+            name="issue"
+            value={issue}
+            onChange={obtenerDatos}
+          />
+        </contenedor>
+        <br></br>
+        <contenedor>
+          <h3>Fecha de incidencia:&nbsp;</h3>
+          <input
+            type="text"
+            name="dateIssue"
+            value={dateIssue}
+            onChange={obtenerDatos}
+          />
+        </contenedor>
+        <br></br>
+        <contenedor>
+          <h3>Mensaje:&nbsp;</h3>
+          <input
+            type="text"
+            name="incidenceContext"
+            value={incidenceContext}
+            onChange={obtenerDatos}
+          />
+        </contenedor>
+        <br></br>
+        {error ? <a>Debes rellenar todos los campos</a> : null}
+        <button type="submit">Enviar</button>
+        </form>
+        </Fragment>
+    )
 };
 export default Incidence;
