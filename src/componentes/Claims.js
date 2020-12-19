95eef9d233de2350646f6617f20afd132f86282d
Alesa <alesaof@gmail.com>
2020-12-19 19:07:09 +0000

    claims devep

--- src/componentes/Claims.js
+++ src/componentes/Claims.js
@@ -6,14 +6,66 @@ const Claims = () => {
   const misReclamaciones = (e) => {
     history.push("/newClaim");
   };
+  const list = document.querySelector("ul");
+
+  const [claimData, setInci] = useState({
+    claim: [],
+  });
+  const { claim } = claimData;
+
+  let clm = [];
+
+  auth.onAuthStateChanged((user) => {
+    if (user) {
+      //var uid = user.uid;
+      var userUID = auth.currentUser.uid;
+
+      var docRef = firestore.collection("usuariosRgistrados").doc(userUID);
+
+      docRef
+        .get()
+        .then(function (doc) {
+          if (doc.exists) {
+            let html = "";
+            console.log("Document data:", doc.data().reclamaciones);
+            doc.data().reclamaciones.forEach((claim) => {
+              html += `
+                <li>
+                  <div>
+                    <h2>Incidencia: ${claim.issue}</h2>
+                    Asunto: ${claim.incidenceContext}
+                    <br></br>
+                    Fecha: ${claim.dateIssue}
+                  </div>
+                </li>
+            `;
+              //list.innerHTML += html;
+
+              ReactDOM.render(
+                <div dangerouslySetInnerHTML={{ __html: html }} />,
+                document.getElementById("reclamaciones")
+              );
+            });
+          } else {
+            console.log("No such document!");
+          }
+        })
+        .catch(function (error) {
+          console.log("Error getting document:", error);
+        });
+    }
+  });
+
   return (
     <Fragment>
       <h1>Mis reclamaciones</h1>
-      <label> AQUI IRIAN LAS RECLAMACIONES</label>
       <br></br>
-      <button onClick={misReclamaciones}> Nueva reclamación</button>
+      <div id="reclamacion">
+      </div>
       <br></br>
-      <Link to={"/logIn"}>Volver</Link>
+      <button onClick={misReclamaciones}> Nueva Reclamacion</button>
+      <br></br>
+      <Link to={"/sesion"}>Volver</Link>
     </Fragment>
   );
 };
