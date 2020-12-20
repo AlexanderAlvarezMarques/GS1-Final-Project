199c5cde5b0843f4ce8ae70473ad9b195312de14
DanielNaranjo <dnartor.98@gmail.com>
2020-12-20 16:16:04 +0000

    Reclaims hecho

--- src/componentes/Claims.js
+++ src/componentes/Claims.js
@@ -1,19 +1,74 @@
-import React, { Fragment } from "react";
-import { Link, useHistory} from "react-router-dom";
+import React, { Fragment, useState } from "react";
+import { Link } from "react-router-dom";
+import { Router, useHistory } from "react-router-dom";
+import { auth, firestore } from "../firebaseConfig";
+import ReactDOM from "react-dom";
 
 const Claims = () => {
   let history = useHistory();
   const misReclamaciones = (e) => {
     history.push("/newClaim");
   };
+  const list = document.querySelector("ul");
+
+  const [claimData, setInci] = useState({
+    claim: [],
+  });
+  const { claim } = claimData;
+
+  let clm = [];
+
+  auth.onAuthStateChanged((user) => {
+    if (user) {
+      //var uid = user.uid;
+      var userUID = auth.currentUser.uid;
+
+      var docRef = firestore.collection("usuariosRgistrados").doc(userUID);
+
+      docRef
+        .get()
+        .then(function (doc) {
+          if (doc.exists) {
+            let html = "";
+            console.log("Document data:", doc.data().reclamaciones); 
+            doc.data().reclamaciones.forEach((claim) => {
+              html += `
+                <li>
+                  <div>
+                    <h2>Reclamación:</h2>
+                    Asunto: ${claim.asunto}
+                    <br></br>
+                    Descripción: ${claim.descripcion}
+                  </div>
+                </li>
+            `;
+              //list.innerHTML += html;
+
+              ReactDOM.render(
+                <div dangerouslySetInnerHTML={{ __html: html }} />,
+                document.getElementById("reclamacion")
+              );
+            });
+          } else {
+            console.log("No such document!");
+          }
+        })
+        .catch(function (error) {
+          console.log("Error getting document:", error);
+        });
+    }
+  });
+
   return (
     <Fragment>
       <h1>Mis reclamaciones</h1>
-      <label> AQUI IRIAN LAS RECLAMACIONES</label>
       <br></br>
-      <button onClick={misReclamaciones}> Nueva reclamación</button>
+      <div id="reclamacion">
+      </div>
       <br></br>
-      <Link to={"/logIn"}>Volver</Link>
+      <button onClick={misReclamaciones}> Nueva Reclamacion</button>
+      <br></br>
+      <Link to={"/sesion"}>Volver</Link>
     </Fragment>
   );
 };
